ec1af87d4e89c76b535cccb0912b4f70380baba8
Stanley Pan <imoment.developer@gmail.com>
2022-01-29 15:21:25 -0500

    Added conditional rendering to CustomTabButton

--- NavSidebar/Components/Sidebar.swift
+++ NavSidebar/Components/Sidebar.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct Sidebar: View {
     @Binding var currentTab: String
     
+    // MARK: Smooth transition with Matched Geometry
+    @Namespace var animation
+    
     var body: some View {
         VStack {
             HStack(spacing: 15) {
@@ -29,10 +32,23 @@ struct Sidebar: View {
             // MARK: Tab Buttons
             VStack(alignment: .leading, spacing: 25) {
                 CustomTabButton(icon: "theatermasks.fill", title: "Home")
+                CustomTabButton(icon: "safari.fill", title: "Discover")
+                CustomTabButton(icon: "applewatch", title: "Devices")
+                CustomTabButton(icon: "person.fill", title: "Profile")
+                CustomTabButton(icon: "gearshape.fill", title: "Setting")
+                CustomTabButton(icon: "info.circle.fill", title: "About")
+                CustomTabButton(icon: "questionmark.circle.fill", title: "Help")
+                
+                Spacer()
+                
+                CustomTabButton(icon: "rectangle.portrait.and.arrow.right", title: "Logout")
             }
             .padding()
             .padding(.top, 60)
+            .frame(width: getScreenSize().width / 2, alignment: .leading)
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
+        .padding(.leading, 10)
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .background(
             Color("background")
@@ -43,15 +59,27 @@ struct Sidebar: View {
     @ViewBuilder
     func CustomTabButton(icon: String, title: String) -> some View {
         Button {
-            
+            if title == "Logout" {
+                print("User has logged out.")
+            } else {
+                withAnimation {
+                    currentTab = title
+                }
+            }
         } label: {
             HStack(spacing: 12) {
                 Image(systemName: icon)
                     .font(.title3)
-                    .frame(width: 48, height: 48)
+                    .frame(width: currentTab == title ? 48 : nil, height: 48)
+                    .foregroundColor(currentTab == title ? Color("purple") : (title == "Logout" ? Color("salmon") : Color.white))
                     .background(
-                        Color.white
-                            .clipShape(Circle())
+                        ZStack {
+                            if currentTab == title {
+                                Color.white
+                                    .clipShape(Circle())
+                                    .matchedGeometryEffect(id: "TABCIRCLE", in: animation)
+                            }
+                        }
                     )
                 
                 Text(title)
@@ -59,17 +87,30 @@ struct Sidebar: View {
                     .fontWeight(.semibold)
                     .foregroundColor(Color.white)
             }
-            .padding(.trailing)
+            .padding(.trailing, 18)
             .background(
-                Color("purple")
-                    .clipShape(Capsule())
+                ZStack {
+                    if currentTab == title {
+                        Color("purple")
+                            .clipShape(Capsule())
+                            .matchedGeometryEffect(id: "TABCAPSULE", in: animation)
+                    }
+                }
             )
         }
+        .offset(x: currentTab == title ? 15 : 0)
     }
 }
 
 struct Sidebar_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// MARK: Getting Screen Bounds
+extension View {
+    func getScreenSize() -> CGRect {
+        return UIScreen.main.bounds
     }
 }
