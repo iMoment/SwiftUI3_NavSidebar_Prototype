6fd3b8a754eba4fae9d564a04fb3848d82892b60
Stanley Pan <imoment.developer@gmail.com>
2022-01-29 14:31:27 -0500

    Initial Commit

---
+++ NavSidebar/NavSidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavSidebarApp.swift
+//  NavSidebar
+//
+//  Created by Stanley Pan on 2022/01/29.
+//
+
+import SwiftUI
+
+@main
+struct NavSidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
